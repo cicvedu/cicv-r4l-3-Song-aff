8161ae80d3284110ef473953de855520ded3e9d0
Song <281218023@qq.com>
2024-04-02 15:32:57 +0800

    Update completion.rs

--- r4l_experiment/driver/002_completion/completion.rs
+++ r4l_experiment/driver/002_completion/completion.rs
@@ -2,61 +2,83 @@
 #![feature(allocator_api, global_asm)]
 
 use kernel::prelude::*;
-use kernel::chrdev::{ChrDev, Cdev, CdevRegistration, File, FileOpener, FileOperations};
 use kernel::sync::completion;
+use kernel::file::File;
+use kernel::file_operations::{FileOpener, FileOperations};
+use kernel::cdev::{Cdev, CdevRegistration};
 
 module! {
-    type: MyModule,
-    name: b"completion_example",
-    author: b"tester",
-    description: b"Example of Kernel's completion mechanism in Rust",
+    type: CompletionModule,
+    name: b"completion",
+    author: b"Tester",
+    description: b"Example of Kernel's completion mechanism",
     license: b"GPL",
 }
 
-struct MyDevice {
+struct CompletionDevice {
+    cdev: Cdev,
     completion: completion::Completion,
 }
 
-impl FileOperations for MyDevice {
+impl FileOperations for CompletionDevice {
     kernel::declare_file_operations!(read, write);
 }
 
-impl FileOpener<MyDevice> for MyDevice {
+impl FileOpener<CompletionDevice> for CompletionDevice {
     fn open(ctx: &kernel::file_operations::FileOpenContext) -> Result<Self::Wrapper> {
-        pr_info!("open() is invoked\n");
-        Ok(MyDevice { completion: completion::Completion::new() }.into())
+        pr_info!("{}() is invoked\n", ctx.name());
+        Ok(CompletionDevice {
+            cdev: Cdev::new(),
+            completion: completion::Completion::new(),
+        }
+        .into())
     }
 }
 
-impl MyDevice {
-    fn read(&self, _file: &File, _data: &mut kernel::user_ptr::UserSlicePtrWriter, _offset: &mut u64) -> Result<usize> {
-        pr_info!("read() is invoked\n");
+impl CompletionDevice {
+    fn read(&self, _file: &File, _buf: &mut [u8], _offset: u64) -> Result<usize> {
+        pr_info!("{}() is invoked\n", "completion_read");
+
+        pr_info!("process {}({}) is going to sleep\n", current_pid(), current_comm());
         self.completion.wait();
-        pr_info!("awoken\n");
+        pr_info!("awoken {}({})\n", current_pid(), current_comm());
+
         Ok(0)
     }
 
-    fn write(&self, _file: &File, _data: &kernel::user_ptr::UserSlicePtrReader, _offset: &mut u64) -> Result<usize> {
-        pr_info!("write() is invoked\n");
+    fn write(&self, _file: &File, _buf: &[u8], _offset: u64) -> Result<usize> {
+        pr_info!("{}() is invoked\n", "completion_write");
+
+        pr_info!("process {}({}) awakening the readers...\n", current_pid(), current_comm());
         self.completion.complete();
-        Ok(0)
+
+        Ok(_buf.len())
     }
 }
 
-struct MyModule {
-    _chrdev: CdevRegistration<MyDevice>,
+struct CompletionModule {
+    chrdev: CdevRegistration<CompletionDevice>,
 }
 
-impl KernelModule for MyModule {
+impl KernelModule for CompletionModule {
     fn init() -> Result<Self> {
-        pr_info!("completion_example module loaded\n");
-        let chrdev = CdevRegistration::new_pinned::<MyDevice>(cstr!("completion_example"), None)?;
-        Ok(MyModule { _chrdev: chrdev })
+        pr_info!("{} module loaded\n", module_name!());
+
+        let chrdev = CdevRegistration::new_pinned::<CompletionDevice>(cstr!("completion"), None)?;
+        Ok(CompletionModule { chrdev })
     }
 }
 
-impl Drop for MyModule {
+impl Drop for CompletionModule {
     fn drop(&mut self) {
-        pr_info!("completion_example module unloaded\n");
+        pr_info!("{} module unloaded\n", module_name!());
     }
+}
+
+fn current_pid() -> i32 {
+    kernel::task::current().pid().into()
+}
+
+fn current_comm() -> &'static str {
+    kernel::task::current().comm()
 }
